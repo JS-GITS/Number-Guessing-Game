c9a28904b3d6c0745724dbdd651f627890ba4f49
Justin <justinesiananda@gmail.com>
2025-07-24 22:08:43 +1000

    Added more comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,34 @@
 
 using namespace std;
 int main() {
-    int guessNum = random_generator(1, 100);
-    print_message();
-    int chances;
+    int guessNum = random_generator(1, 100);    // The correct number the user has to guess
+    print_message();                            // Displays the welcome and difficulty selection menu
+
+    int chances;                                // Number of attempts user will get, based on difficulty
+
     do {
         cout << "Enter your choice: ";
-        chances = selection();
+        chances = selection();                  // Requests the user to pick a difficulty
     } while (chances == 0);
-    initiate(chances, guessNum);
+
+    initiate(chances, guessNum);                // Starts the game
+
+    // If difficulty is "Practice" mode (unlimited tries), offer replay loop
     while (chances == INT_MAX) {
         char c;
-        cout << "Would you like to play another round? (y/n) ";
+        cout << "Would you like to play another round? (y/n) "; // Asks the user if they want to play again if on practice mode
         cin >> c;
         if (c == 'y') {
-            guessNum = random_generator(1, 100);
-            initiate(chances,guessNum);
+            guessNum = random_generator(1, 100);                // Generate a new number
+            initiate(chances,guessNum);                         // Restart the game
         }
         else if(c == 'n') {
-            break;
+            break;  // Exit the loop and program
         }
         else {
-            cerr << "You must input either \'y\' or \'n\'.\n";
+            cerr << "You must input either 'y' or 'n'.\n\n";
         }
     }
-    cout << "Exiting game..." << endl;
+    cout << "Exiting game..." << endl;                          // Prints this message when the program is about to end
     return 0;
 }
